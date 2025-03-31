c4c1d49ec2994a9cbec8c01524e6f113741755bc
foce123 <foce123@sina.com>
2025-03-31 19:49:52 +0800

    init

---
+++ flux-backend/app.py
@@ -0,0 +1,15 @@
+import uvicorn
+from uvicorn.workers import UvicornWorker
+
+from server import app, AppConfig  # noqa: F401
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app='app:app',
+        host=AppConfig.app_host,
+        port=AppConfig.app_port,
+        workers=AppConfig.app_workers,
+        reload=AppConfig.app_reload,
+    )
+UvicornWorker
